cd16d104170e8f267db72caa8cfed6a10dd87e9f
Cedric Fung <cedric@vec.io>
2021-03-09 22:11:17 +0800

    fix typo

--- src/utils/mixin.js
+++ src/utils/mixin.js
@@ -13,7 +13,6 @@ Mixin.prototype = {
   },
 
   conversationId: function () {
-    return "4482a2e7-9b28-4999-b9b4-6173aa41e107";
     switch (this.environment()) {
       case 'iOS':
         var ctx = prompt('MixinContext.getContext()');
