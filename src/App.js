7aae1263acc39899641e6f596f7731238f565ef0
Li Yuqing <im.yuqlee@gmail.com>
2021-04-27 08:37:02 +0800

    Add guide page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import './App.css';
+
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from './home/view.js';
+import Guide from './guide/view.js';
 
-import './App.css';
 
 export default function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={ Home.Index } />
+        <Route exact path="/guide" component={ Guide.Index } />
       </Switch>
     </Router>
   );
--- src/guide/index.js
+++ src/guide/index.js
@@ -1,9 +1,15 @@
+import styles from './index.module.scss';
+import { ReactComponent as Guide } from '../statics/images/ic_guide.svg';
+
 function Index() {
+  const i18n = window.i18n;
+
   return (
-    <div>
-      <h2>Home</h2>
+    <div className={ styles.guide }>
+      <Guide className={ styles.icon } />
+      <ul className={ styles.body } dangerouslySetInnerHTML={{__html: i18n.t('guide')}}></ul>
     </div>
   );
 }
 
-export default Index
+export default Index;
