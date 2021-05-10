83fa32c707cd809b06ebc5d82735b202056228f3
Li Yuqing <im.yuqlee@gmail.com>
2021-05-10 14:12:19 +0800

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm -rf ./build
+mkdir build
+
+git pull
+yarn install && yarn build
+
+SUM=`md5 -q build/index.html`
+mv build/index.html build/index.$SUM.html
+
+cp app.yaml build/app.yaml
+sed -i ''  "s/index.html/index.$SUM.html/g" build/app.yaml || exit
+cd build && gcloud app deploy app.yaml -y
